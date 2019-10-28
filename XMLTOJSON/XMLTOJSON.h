1bdc9f16cbfc8c182bbac7e0882856113cba48f3
myCRObaM <matej.hetzel@gmail.com>
2019-10-28 08:01:15 +0100

    Initial Commit

---
+++ XMLTOJSON/XMLTOJSON.h
@@ -0,0 +1,19 @@
+//
+//  XMLTOJSON.h
+//  XMLTOJSON
+//
+//  Created by Matej Hetzel on 28/10/2019.
+//  Copyright © 2019 Matej Hetzel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XMLTOJSON.
+FOUNDATION_EXPORT double XMLTOJSONVersionNumber;
+
+//! Project version string for XMLTOJSON.
+FOUNDATION_EXPORT const unsigned char XMLTOJSONVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XMLTOJSON/PublicHeader.h>
+
+
